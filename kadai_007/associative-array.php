cf9df4873718e37b104f7981662e38c96e7faf82
iamtaniyuki <tm5.yk.919@gmail.com>
2025-02-08 22:43:34 +0900

    upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>テストの平均点を計算しよう</title>
+</head>
+
+<body>
+   <p>
+        <?php
+           // 連想配列
+           $item1 = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+           print_r($item1);
+        ?>
+   </p>
+</body>
+
+</html>
